341a1c7f1ffe454ee9fbf51ac1c6caf9ee7dcceb
Md Suhail <mohammedsuhail55410@gmail.com>
2026-04-25 04:08:53 +0530

    project

--- medium_2nd/backend/src/index.ts
+++ medium_2nd/backend/src/index.ts
@@ -1,60 +1,22 @@
 import { Hono } from 'hono';
 import { PrismaClient } from './generated/prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
-import { decode, sign, verify } from 'hono/jwt'
-// Define the env bindings for Cloudflare Workers
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
 const app = new Hono<{
   Bindings: {
     ACCELERATE_URL: string;
+    JWT_SECRET: string;
   },
   Variables: {
     prisma: any
   }
 }>();
 
-app.use('*', async (c, next) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.ACCELERATE_URL,
-  }).$extends(withAccelerate());
-  c.set('prisma', prisma);
-  await next();
-});
 
-app.post('/api/v1/signup', async (c) => {
-  const prisma = c.get('prisma');
+app.route('/api/v1/user', userRouter);
+app.route('/api/v1/blog', blogRouter);
 
-  const body = await c.req.json();
 
-  const user = await prisma.user.create({
-    data: {
-      email: body.email,
-      name: body.name,
-      password: body.password
-    }
-  });
-
-  const mysecert = "medium-blog-pool"
-  const token = await sign({ id: user.id }, mysecert)
-
-  return c.json({ jwt: token });
-});
-
-app.post('/api/v1/signin', (c) => {
-  return c.text('signin route');
-});
-
-app.get('/api/v1/blog/:id', (c) => {
-  const id = c.req.param('id');
-  console.log(id);
-  return c.text('get blog route');
-});
-
-app.post('/api/v1/blog', (c) => {
-  return c.text('signin route');
-});
-
-app.put('/api/v1/blog', (c) => {
-  return c.text('signin route');
-});
 
 export default app;
